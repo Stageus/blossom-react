db69a64a60ca9bcfc4d8063aa78a09117bb0e2db
haewonwon <haewon@gimhaewon-ui-MacBookPro.local>
2024-04-09 16:12:01 +0900

    Feat & Style: LoginForm Style 및 Login page 로직 구현 1. LoginForm 로직 구현 미완 (Modal, Validation 등 구현 예정

--- src/styles/TextStyle.js
+++ src/styles/TextStyle.js
@@ -21,3 +21,15 @@ export const Label = styled.label`
   margin: ${(props) => props.margin || "0"};
   padding: ${(props) => props.padding || "0"};
 `;
+
+export const Span = styled.span`
+  /* colors */
+  color: ${(props) => props.textColor || "#32250F"};
+
+  /* fonts */
+  font-size: ${(props) => props.fontSize || "18px"};
+
+  /* spacing */
+  margin: ${(props) => props.margin || "0"};
+  padding: ${(props) => props.padding || "0"};
+`;
