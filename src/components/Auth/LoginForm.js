8040233a17c21ddc3bdbbeaabb691e51992f8492
haewonwon <haewon@gimhaewon-ui-MacBookPro.local>
2024-04-13 20:50:17 +0900

    Code: utils 폴더 셍성 및 validation util function 생성

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -12,6 +12,16 @@ const LoginForm = () => {
   const IdRef = useRef("");
   const passwordRef = useRef("");
 
+  useEffect(() => {
+    // 로그인 실패 시, Error Message 출력
+    // 로그인 성공 시, 초기 설정 페이지 혹은 메인 페이지로 이동
+  }, [response]);
+
+  const handleLogin = () => {
+    // 아이디, 비밀번호 유효성 검사
+    // 로그인 API 통신 코드
+  };
+
   return (
     <>
       {/* 로그인 Input Field */}
@@ -21,7 +31,7 @@ const LoginForm = () => {
       </FlexBox>
 
       {/* 로그인 Button */}
-      <Button width="25.625rem" height="5rem">
+      <Button width="25.625rem" height="5rem" onClick={handleLogin}>
         로그인
       </Button>
     </>
