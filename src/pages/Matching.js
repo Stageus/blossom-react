ac96e4a8547ddb31026ef1f5a43663046380c222
haewonwon <khcw1029@daum.net>
2024-04-22 02:35:19 +0900

    Feat: Mathcing 기능 구현 및 페이지 구현

--- src/pages/Matching.js
+++ src/pages/Matching.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+// ===== stlyes import =====
+import FlexBox from "../styles/FlexStyle";
+import P, { H1 } from "../styles/TextStyle";
+
+// ===== components import =====
+import MatchingForm from "../components/Setting/MatchingForm";
+
+// ===== component =====
 const Matching = () => {
-  return <h1>상대 확인 페이지</h1>;
+  return (
+    <FlexBox $row="end" $width="100%">
+      <FlexBox
+        $dir="col"
+        $row="center"
+        $col="center"
+        $width="32.438rem"
+        $height="100vh;"
+        $backgroundColor="#FBE8E7"
+      >
+        {/* 매칭 Header */}
+        <FlexBox $dir="col" $width="25.625rem" $margin="0 0 30px 0">
+          <H1>매칭하기</H1>
+          <P $fontSize="16px">매칭을 희망하는 상대방의 아이디를 입력해 주세요.</P>
+        </FlexBox>
+
+        {/* 매칭 Form */}
+        <MatchingForm />
+      </FlexBox>
+    </FlexBox>
+  );
 };
 
 export default Matching;
